b45aa7c1fc2f3a4022f50952d2c5f21036710199
Mrinal Shinde <mrinalgoa@gmail.com>
2017-03-12 01:42:51 +0530

    trying local minima

--- StaticFeatures/johnstride.m
+++ StaticFeatures/johnstride.m
@@ -1,17 +1,11 @@
 clear all;
-load stride.txt;
-x = stride;
+load johnstride.txt;
+x = johnstride;
 a = 1;
 b = [1/4 1/4 1/4 1/4];
 y = filter(b,a,x);
 t = 1:length(x);
-w = y(4:75,1);
 
-
-
-
-
-
-plot(t,x,'',t,w,'-');
+plot(t,x,'',t,y,'-');
 legend('original','mask is 1/4');
 
